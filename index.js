2f496ab640a7594d34766ff70de6df3201e1e860
otienosteve <steveotieno701@gmail.com>
2022-04-23 11:53:43 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+
+
+let num1=1
+let num2=62
+
+
+let multiply=num1*num2
+
+
+let num3=10
+let num4=6
+let random=Math.floor(Math.random()*10)
+let mod=num3%num4
+let max=Math.max(10,20,5,7)
